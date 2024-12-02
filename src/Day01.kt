0b8c2c4359aef7db17bdc0e3e26e1cb72eb43ec6
Jeremy Ketterer <dev.jketterer@gmail.com>
2024-12-02 10:36:32 -0500

    refactor: wrap day 1 in unique object to avoid future import conflicts

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,5 @@
-import kotlin.math.abs
-import Part1.calculateTotalDistance
-import Part2.calculateSimilarityScore
+import Day01.Part1.calculateTotalDistance
+import Day01.Part2.calculateSimilarityScore
 import kotlin.math.absoluteValue
 
 // https://adventofcode.com/2024/day/1
@@ -10,40 +9,43 @@ fun main() {
     println("Part 2: ${calculateSimilarityScore(input)}")
 }
 
-private object Part1 {
-    fun calculateTotalDistance(input: List<String>): Int {
-        val (left, right) = buildLists(input)
-        val sortedLeft = left.sorted()
-        val sortedRight = right.sorted()
-        return sortedLeft.mapIndexed { i, num ->
-            (num - sortedRight[i]).absoluteValue
-        }.sum()
-    }
-}
-
-private object Part2 {
-    fun calculateSimilarityScore(input: List<String>): Int {
-        val (left, right) = buildLists(input)
-        val occurrencesMap: MutableMap<Int, Int> = mutableMapOf()
-        return left.sumOf { num ->
-            val occurrences = occurrencesMap.computeIfAbsent(num) { i -> right.count { it == i } }
-            occurrences * num
+private object Day01 {
+    object Part1 {
+        fun calculateTotalDistance(input: List<String>): Int {
+            val (left, right) = buildLists(input)
+            val sortedLeft = left.sorted()
+            val sortedRight = right.sorted()
+            return sortedLeft.mapIndexed { i, num ->
+                (num - sortedRight[i]).absoluteValue
+            }.sum()
         }
     }
-}
 
-private fun buildLists(input: List<String>): Pair<List<Int>, List<Int>> {
-    val firsts: MutableList<Int> = mutableListOf()
-    val seconds: MutableList<Int> = mutableListOf()
-
-    input.forEachIndexed { i, s ->
-        val pair = s.split("   ")
-        if (pair.size > 2) {
-            throw IllegalArgumentException("Line ${i + 1} contains more than two numbers: $pair")
+    object Part2 {
+        fun calculateSimilarityScore(input: List<String>): Int {
+            val (left, right) = buildLists(input)
+            val occurrencesMap: MutableMap<Int, Int> = mutableMapOf()
+            return left.sumOf { num ->
+                val occurrences =
+                    occurrencesMap.computeIfAbsent(num) { i -> right.count { it == i } }
+                occurrences * num
+            }
         }
-        firsts.add(pair.first().toInt())
-        seconds.add(pair.last().toInt())
     }
 
-    return Pair(firsts.toList(), seconds.toList())
+    private fun buildLists(input: List<String>): Pair<List<Int>, List<Int>> {
+        val firsts: MutableList<Int> = mutableListOf()
+        val seconds: MutableList<Int> = mutableListOf()
+
+        input.forEachIndexed { i, s ->
+            val pair = s.split("   ")
+            if (pair.size > 2) {
+                throw IllegalArgumentException("Line ${i + 1} contains more than two numbers: $pair")
+            }
+            firsts.add(pair.first().toInt())
+            seconds.add(pair.last().toInt())
+        }
+
+        return Pair(firsts.toList(), seconds.toList())
+    }
 }
